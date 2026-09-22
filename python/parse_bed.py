ef0d9238d687877897303ca6fb59225f92e044c8
anitahn246 <nakavumaanitah@gmail.com>
2026-09-22 23:26:02 +0300

    Add BED file parser to calculate read counts and average quality
    - Implemented a new script  to read a BED file and calculate the number of reads for each chromosome.
    - Added a menu for users to display read counts or exit the program.
    - Included functionality to skip comment lines in the BED file.
    - Prepared the structure for future implementation of average read quality calculation.

---
+++ python/parse_bed.py
@@ -0,0 +1,74 @@
+#The program requesting from the bed file from the user
+#for each chromosome calculate the number of reads 
+# calculate the average read quality
+#load the menu DONE
+#Select an option
+  #[R] Display read counts in each Chromosome
+  #[D] Display the average read quality for each chromosome
+  #[X] Exit
+  
+  #below is the logic for the program me
+  
+#add a comment
+counts={}
+bed_file=input("Please enter the name of the bed file: ")
+
+for line in open(bed_file):
+    line=line.strip()
+    
+    if line.startswith("#"): 
+        continue
+    
+    columns=line.split()
+    
+    chromosome=columns[0]
+    
+    if chromosome not in counts: 
+        counts[chromosome]=1
+    else:
+        counts[chromosome] = counts[chromosome]+1 
+bed_file.close() 
+
+    
+    
+    
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+while True: 
+    print("Select an option")
+    print("[R] Display read counts in each Chromosome")
+    print("[D] Display the average read quality for each chromosome")
+    print("[X] Exit")
+    option=input("Please select an option from the menu: ").upper()
+    if option=="R":
+        sorted_chromosomes = sorted(counts,key=lambda chr:counts[chr], reverse=True)
+        print("columns", sorted_chromosomes)
+        
+    elif option=="D":
+        print("D")
+    elif option=="X":
+        print("You have exited the program")  
+        break   
+    else:
+        print("Invalid option, please select a valid option from the menu")
+        
+        
